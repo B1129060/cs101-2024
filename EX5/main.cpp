896c64eac83f06e2049d5a5030dd202ad32726bf
B1129060 <91174554+B1129060@users.noreply.github.com>
2024-05-23 13:59:12 +0800

    Create main.cpp

---
+++ EX5/main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class myString {
+	private: 
+		string m_str;
+	public:
+		myString(string s){
+			m_str = s;
+		}		
+};
+
+class ReadClass {
+	private:
+		string filename;
+	public:
+		ReadClass(){
+			filename = "main.cpp";
+		}
+		
+		void showClass();{
+			vector<string> name;
+			string s;
+			int num = 0;
+			ifstream in;
+			in.open(this->filename.c_str());
+			if(in.fail()){
+				cout << "fail" << endl;
+				return;
+			}
+			while(!in.eof()){
+				in >> s;
+				if(s == "class"){
+					in >> s;
+					name.push_back(s);
+					num++;
+				}
+			}
+			in.close();
+			cout << --num << " class in main.cpp\n";
+			for(int i = 0; i < name.size() - 1; i++){
+				cout << "class " << name[i] << endl;
+			}
+		}
+		
+};
+
+int main(){
+    ReadClass rfile;
+    rfile.showClass();
+    return 0;
+    
+}
+
